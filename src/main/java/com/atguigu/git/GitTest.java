a5460c5e3e58885b008daea1d678f6e10179edd1
zkg <771505413@qq.com>
2021-12-12 07:04:18 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,6 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
-        System.out.println("hello git4");
+        System.out.println("hello git5");
     }
 }
