3e346701324d2668e9e98d1794952d989704cf33
zkg <771505413@qq.com>
2021-12-12 06:53:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 周科港
+ * @Date: 2021/12/12 6:49
+ * Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
