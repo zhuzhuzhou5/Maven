64770975e6a6298a2a1c09b941a45b653585a5d9
zkg <771505413@qq.com>
2021-12-12 06:55:42 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
